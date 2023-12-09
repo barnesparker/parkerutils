885e85e53444beb495c31560a85dd15e48f41366
Parker Barnes <barnesparker1@gmail.com>
2023-12-09 00:14:31 -0700

    add test for search_tbl

--- tests/testthat/test-search_tbl.R
+++ tests/testthat/test-search_tbl.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("it works", {
+  test_tbl <-
+    tibble::tibble(
+      x = c("a", "b", "c"),
+      y = c("d", "e", "a"),
+      z = c("f", "g", "h")
+    )
+
+  expect_equal(
+    test_tbl |>
+      search_tbl("a"),
+    tibble::tibble(
+      x = c("a", "c"),
+      y = c("d", "a")
+    )
+  )
+
+
 })
